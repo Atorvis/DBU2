5830aab5de3014b2967dfff97e7f5d2708274488
shubham3000 <shubhamsadhu02@gmail.com>
2022-02-24 12:07:29 +0530

    new commit

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 //for toogle header
 
-window.addEventListener('scroll', function(){
-    var header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-});
-
-
 function togglemenu(){
     var menuToggle = document.querySelector('.toggle');
     var menu = document.querySelector('.menu');
